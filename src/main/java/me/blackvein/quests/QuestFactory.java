028079d8859ed42b21036dac9b19d866d0bc3ab7
Zino <zinoonomiwo@gmail.com>
2013-08-06 21:35:17 +0200

    *fix for quests who contain a same part in the name *easier to select quests to edit.

--- src/main/java/me/blackvein/quests/QuestFactory.java
+++ src/main/java/me/blackvein/quests/QuestFactory.java
@@ -391,11 +391,29 @@ public class QuestFactory implements ConversationAbandonedListener, ColorUtil {
 
                 for (Quest q : quests.getQuests()) {
 
-                    if (q.getName().equalsIgnoreCase(input) || q.getName().toLowerCase().contains(input.toLowerCase())) {
+                    if (q.getName().equalsIgnoreCase(input)) {
                         loadQuest(context, q);
                         return new CreateMenuPrompt();
                     }
+                    
+                }
+                
+                for (Quest q : quests.getQuests()) {
 
+                    if (q.getName().toLowerCase().startsWith(input.toLowerCase())) {
+                        loadQuest(context, q);
+                        return new CreateMenuPrompt();
+                    }
+                    
+                }
+                
+                for (Quest q : quests.getQuests()) {
+
+                    if (q.getName().toLowerCase().contains(input.toLowerCase())) {
+                        loadQuest(context, q);
+                        return new CreateMenuPrompt();
+                    }
+                    
                 }
 
                 return new SelectEditPrompt();
