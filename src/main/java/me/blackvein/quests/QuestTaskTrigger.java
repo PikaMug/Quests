1e3c2365a3d682381856d458a8e427adc4e33267
Zino <zinoonomiwo@gmail.com>
2013-08-09 21:56:24 +0200

    * Update to latest version of Denizens (0.9.2)

--- src/main/java/me/blackvein/quests/QuestTaskTrigger.java
+++ src/main/java/me/blackvein/quests/QuestTaskTrigger.java
@@ -1,17 +1,18 @@
 package me.blackvein.quests;
 
+import net.aufdemrand.denizen.objects.dPlayer;
 import net.aufdemrand.denizen.scripts.ScriptRegistry;
 import net.aufdemrand.denizen.scripts.containers.core.TaskScriptContainer;
 import org.bukkit.entity.Player;
 
 public class QuestTaskTrigger {
 
-    public boolean parseQuestTaskTrigger(String theScriptName, Player thePlayer) {
+    public boolean parseQuestTaskTrigger(String theScriptName, Player player) {
         if (!ScriptRegistry.containsScript(theScriptName)) {
             return false;
         }
         TaskScriptContainer task_script = ScriptRegistry.getScriptContainerAs(theScriptName, TaskScriptContainer.class);
-        task_script.runTaskScript(thePlayer, null, null);
+        task_script.runTaskScript(dPlayer.mirrorBukkitPlayer(player), null, null);
         return true;
     }
 }
