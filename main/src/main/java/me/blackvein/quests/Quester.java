15db4cc9968db85d1547fff5e7514c7ead3337e1
PikaMug <unconfigured@null.spigotmc.org>
2019-09-27 02:33:16 -0400

    Revert 654a8e89620d67750a7f0e783b1f0aae5b64c788

--- main/src/main/java/me/blackvein/quests/Quester.java
+++ main/src/main/java/me/blackvein/quests/Quester.java
@@ -236,12 +236,7 @@ public class Quester {
 	}
 
 	public void setQuestToTake(String questToTake) {
-		Quester q = this;
 		this.questToTake = questToTake;
-		if (plugin.getQuesters().contains(q)) {
-			plugin.getQuesters().remove(q);
-			plugin.getQuesters().add(this);
-		}
 	}
 
 	public int getQuestPoints() {
