b04bc5d54c4bcad6cbb947209d83235e37cf384d
PikaMug <unconfigured@null.spigotmc.org>
2019-11-13 13:40:57 -0500

    Event class

---
+++ main/src/main/java/me/blackvein/quests/events/editor/quests/QuestsEditorPostOpenSelectCreatePromptEvent.java
@@ -0,0 +1,22 @@
+package me.blackvein.quests.events.editor.quests;
+
+import org.bukkit.conversations.ConversationContext;
+import org.bukkit.event.HandlerList;
+
+public class QuestsEditorPostOpenSelectCreatePromptEvent extends QuestsEditorEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    public QuestsEditorPostOpenSelectCreatePromptEvent(ConversationContext context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
