40a7ae0cff3dd2e2bc2fb6e583c0030568d83ddd
PikaMug <2267126+PikaMug@users.noreply.github.com>
2020-08-20 06:10:41 -0400

    Make loadable item programmatically accessible

--- main/src/main/java/me/blackvein/quests/convo/generic/ItemStackPrompt.java
+++ main/src/main/java/me/blackvein/quests/convo/generic/ItemStackPrompt.java
@@ -668,7 +668,7 @@ public class ItemStackPrompt extends QuestsEditorNumericPrompt {
         }
     }
 
-    private String getItemData(final ConversationContext context) {
+    public String getItemData(final ConversationContext context) {
         if (context.getSessionData("tempName") != null) {
             String item;
             if (context.getSessionData("tempDisplay") == null) {
