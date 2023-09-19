d084b1204f5f04d9f4a32e67a32006fd3e5742e2
PikaMug <2267126+PikaMug@users.noreply.github.com>
2023-09-19 04:12:11 -0400

    Do not assume click index, fixes #2143

--- core/src/main/java/me/pikamug/quests/listeners/BukkitCitizensListener.java
+++ core/src/main/java/me/pikamug/quests/listeners/BukkitCitizensListener.java
@@ -201,9 +201,12 @@ public class BukkitCitizensListener implements Listener {
                     if (quester.getCurrentStage(quest).getNpcsToInteract().contains(event.getNPC().getUniqueId())) {
                         final int npcIndex = quester.getCurrentStage(quest).getNpcsToInteract().indexOf(event.getNPC()
                                 .getUniqueId());
-                        if (quester.getQuestDataOrDefault(quest) != null && npcIndex > -1
-                                && !((BukkitQuestProgress) quester.getQuestDataOrDefault(quest)).npcsInteracted.get(npcIndex)) {
-                            hasObjective = true;
+                        if (quester.getQuestDataOrDefault(quest) != null && npcIndex > -1) {
+                            final LinkedList<Boolean> interacted
+                                    = ((BukkitQuestProgress) quester.getQuestDataOrDefault(quest)).npcsInteracted;
+                            if (interacted.size() > npcIndex && !interacted.get(npcIndex)) {
+                                hasObjective = true;
+                            }
                         }
                         quester.interactWithNPC(quest, event.getNPC().getUniqueId());
                     }
