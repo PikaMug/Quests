a47155007984b9765fefdce949db2f7f686f7d62
MrGraycat <moorsn@gmail.com>
2024-01-12 09:43:13 +0100

    Fix NPE error

--- core/src/main/java/me/pikamug/quests/player/BukkitQuester.java
+++ core/src/main/java/me/pikamug/quests/player/BukkitQuester.java
@@ -4346,7 +4346,7 @@ public class BukkitQuester implements Quester {
      */
     public void hardQuit(final Quest quest) {
         try {
-            if (compassTargetQuestId.equals(quest.getId())) {
+            if (compassTargetQuestId != null && compassTargetQuestId.equals(quest.getId())) {
                 compassTargetQuestId = null;
             }
             currentQuests.remove(quest);
